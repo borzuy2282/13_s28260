6fb52e343757532ec6bf31f320dc7789932cb3a6
Borzixx <tim.barzashvili@gmail.com>
2024-03-18 12:39:35 +0100

    Task 1-4
    Have done tasks 1-4

---
+++ PPY_Task_03/main.py
@@ -0,0 +1,33 @@
+#Task 1
+import math
+
+list1 = [i ** 2 for i in range(1, 11)]
+
+print(list1)
+
+
+#Task 2
+
+def list_square_creator(start, end):
+    return [i ** 2 for i in range(start, end)]
+
+print(list_square_creator(1, 11))
+
+
+#Task 3
+
+class SquareGenerator:
+    def list_square_creating(start, end):
+        return [i**2 for i in range(start, end)]
+
+print(SquareGenerator.list_square_creating(1, 11))
+
+
+#Task 4
+
+list4 = SquareGenerator.list_square_creating(1, 11)
+for x in range(len(list4)):
+    list4[x] = math.sqrt(list4[x])
+print(list4)
+
+
